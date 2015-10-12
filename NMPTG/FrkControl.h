f6a7de353023144f8273b4b751fa0f5c64c87e20
hotkutepro <hotkutepro@gmail.com>
2015-10-12 08:59:31 +0700

    Thêm class FrkControl.h

---
+++ NMPTG/FrkControl.h
@@ -0,0 +1,11 @@
+#ifndef FrkControl
+#include"FrkContent.h"
+#include"FrkGraphic.h"
+#include"FrkKeyboard.h"
+#define FrkControl
+
+FrkGraphic* m_hGraphic;
+FrkContent* m_hContent;
+FrkKeyboard* m_hKeyboard;
+
+#endif FrkControl
